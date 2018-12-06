a8cb1ca3d8637fc4fc169d9965d33f8526ecfcff
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-06 19:56:17 +0100

    Added redux function deleteTask

--- src/store/reducers/todoListReducer.js
+++ src/store/reducers/todoListReducer.js
@@ -1,8 +1,13 @@
 import * as TodoListAction from '../actions/todoListActions';
 
+const idGenerator = () => {
+	return '_' + Math.random().toString(36).substr(2, 9);
+};
+
 const initialState = {
 	tasks: [
 		{
+			id: idGenerator(),
 			name: 'zrob bulki',
 			description: 'musisz zrobic bulki gosciu'
 		}
@@ -14,6 +19,9 @@ export function todoReducer(state = initialState, action) {
 		case TodoListAction.ADD_TASK: {
 			const task = { name: action.payload.name, description: action.payload.description };
 			return { ...state, tasks: [ ...state.tasks, task ] };
+		}
+		case TodoListAction.DELETE_TASK: {
+			return { ...state, tasks: [ state.task.filter((id) => state.task.id === id) ] };
 		}
 
 		default: {

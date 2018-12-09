4a575e0ec65c24f4b84b812a2eb03a9acb797dce
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-09 21:13:29 +0100

    Added buttons from primereact to Task component.

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { deleteTaskAction, showTaskAction } from '../../store/actions/todoListActions';
 import { connect } from 'react-redux';
 import {Component } from 'react';
+import {Button} from 'primereact/button';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 class Task extends Component {
 	constructor(props) {
@@ -13,9 +18,9 @@ class Task extends Component {
 			<div className="Task">
 				<p>{this.props.name}</p>
 				<p>{this.props.description}</p>
-				<button onClick={() => this.props.showTask(this.props)}>Show</button>
-				<button onClick={() => this.props.showTask(this.props)}>Edit</button>
-				<button onClick={() => this.props.deleteTask(this.props)}>Delete</button>
+				<Button label="Show" icon="pi pi-info" className="p-button-success" onClick={() => this.props.showTask(this.props)}/>
+				<Button label="Edit" icon="pi pi-spinner" onClick={() => this.props.showTask(this.props)}/>
+				<Button label="Delete" icon="pi pi-ban" className="p-button-danger" onClick={() => this.props.showTask(this.props)}/>
 			</div>
 		);
 	}
